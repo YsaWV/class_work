4c8f8f988aa63963d3a3b3d15f43b096d13285a4
jacob <2staffi@gmail.com>
2016-09-06 18:23:58 -0400

    got working again

--- wk4/lottery2.rb
+++ wk4/lottery2.rb
@@ -1,9 +1,13 @@
 def off(my_ticket, winning_tickets)
 	i = 0
+	c = 0
 	4.times do
-		if my_ticket[i] = winning_tickets[0][i]
-			i += 1
+		if my_ticket[i] == winning_tickets[0][i]			
+			c += 1
 		end
+		i += 1
 	end
-	false
+		if c == 4
+			false
+		end
 end
--- wk4/lottery2_test.rb
+++ wk4/lottery2_test.rb
@@ -11,6 +11,6 @@ class TestLotteryNumbers <Minitest::Test
     # def test_ticket_off_by_1_returns_true
     #     my_ticket = "4456"
     #     winning_ticket = "5456"
-    #     assert_equal(true, big_lotto(my_ticket, winning_ticket))
+    #     assert_equal(true, off(my_ticket, winning_ticket))
     # end
 end
