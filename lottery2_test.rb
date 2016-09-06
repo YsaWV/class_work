ef3d82fc66eb35319d37ec181a505a5f9744d16f
jacob <2staffi@gmail.com>
2016-09-06 15:47:58 -0400

    init commit

--- lottery2_test.rb
+++ lottery2_test.rb
@@ -8,9 +8,9 @@ class TestLotteryNumbers <Minitest::Test
         assert_equal(false, big_lotto(my_ticket, winning_tickets))
     end
 
-    def test_ticket_off_by_1_returns_true
-        my_ticket = "4456"
-        winning_ticket = "5456"
-        assert_equal(true, big_lotto(my_ticket, winning_ticket))
-    end
+    # def test_ticket_off_by_1_returns_true
+    #     my_ticket = "4456"
+    #     winning_ticket = "5456"
+    #     assert_equal(true, big_lotto(my_ticket, winning_ticket))
+    # end
 end
