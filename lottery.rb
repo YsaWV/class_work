f7a7c0d33db2366d697febc634ee9352b0cbbcc5
jacob <2staffi@gmail.com>
2016-09-06 15:49:41 -0400

    delete

--- lottery.rb
+++
@@ -1,9 +0,0 @@
-def grand_lotto(winners, my_num)
-	matches = []
-		if winners.include?(my_num)
-		matches << my_num
-		end
-	matches
-end
-
-
--- lottery2.rb
+++
@@ -1,7 +0,0 @@
-def big_lotto(my_ticket, winning_tickets)
-	matches = []
-		if winning_tickets.include?(my_ticket)
-		matches << my_ticket
-		end
-	matches
-end
