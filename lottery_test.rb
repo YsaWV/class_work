5b2381df4977f98945e98ab088f7ee3a3a839d42
jacob <2staffi@gmail.com>
2016-09-06 15:06:15 -0400

    test return winner

--- lottery_test.rb
+++ lottery_test.rb
@@ -13,4 +13,11 @@ class TestLotteryNumbers <Minitest::Test
 		my_num = "5447"
 		assert_equal(false, grand_lotto(winners, my_num))
 	end
+
+	def test_return_winners
+		winners = ["1234", "2567", "1984", "3756"]
+		my_num = "2567"
+		assert_equal("2567", grand_lotto(winners, my_num))
+	end
+
 end
