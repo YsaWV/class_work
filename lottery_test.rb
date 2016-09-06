9edaddb054704a7bd4348651cd090d8b90267cbe
jacob <2staffi@gmail.com>
2016-09-06 14:15:07 -0400

    fixed loser test

--- lottery_test.rb
+++ lottery_test.rb
@@ -1,9 +1,16 @@
 require "minitest/autorun"
 require_relative "lottery.rb"
 
+class TestAdditionFunction <Minitest::Test
+	def test_to_check_winner
+		winners = ["1234", "2567", "1984", "3756"]
+		my_num = "2567"
+		assert_equal(true, grand_lotto(winners, my_num))
+	end
 
-def test_to_check_winner
-winners = ["1234", "2567", "1984", "3756"]
-my_num = "2567"
-assert_equal(true, grand_lotto(winners, my_num))
+	def test_to_check_loser
+		winners = ["1234", "2567", "1984", "3756"]
+		my_num = "5447"
+		assert_equal(false, grand_lotto(winners, my_num))
+	end
 end
