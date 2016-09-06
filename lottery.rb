fe12522da9a3ac156503ef09b71631795198caf4
jacob <2staffi@gmail.com>
2016-09-06 15:20:28 -0400

    removed frue/false return empty

--- lottery.rb
+++ lottery.rb
@@ -1,5 +1,9 @@
 def grand_lotto(winners, my_num)
-	winners.include?(my_num)
-
-
+	matches = []
+		if winners.include?(my_num)
+		matches << my_num
+		end
+	matches
 end
+
+
--- lottery_test.rb
+++ lottery_test.rb
@@ -5,19 +5,19 @@ class TestLotteryNumbers <Minitest::Test
 	def test_to_check_winner
 		winners = ["1234", "2567", "1984", "3756"]
 		my_num = "2567"
-		assert_equal(true, grand_lotto(winners, my_num))
+		assert_equal(["2567"], grand_lotto(winners, my_num))
 	end
 
 	def test_to_check_loser
 		winners = ["1234", "2567", "1984", "3756"]
 		my_num = "5447"
-		assert_equal(false, grand_lotto(winners, my_num))
+		assert_equal([], grand_lotto(winners, my_num))
 	end
 
 	def test_return_winners
 		winners = ["1234", "2567", "1984", "3756"]
 		my_num = "2567"
-		assert_equal("2567", grand_lotto(winners, my_num))
+		assert_equal(["2567"], grand_lotto(winners, my_num))
 	end
 
 end
