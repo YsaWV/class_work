b45e312eeb29d93f912373e217ae92f5f3e48302
jacob <2staffi@gmail.com>
2016-09-07 21:24:04 -0400

    split code into working and tinker

--- lottery2.rb
+++ lottery2.rb
@@ -1,14 +1,11 @@
 def off(my_ticket, winning_tickets)
 	i = 0
 	c = 0
-	winning_tickets.each do |element|
-		next if element = nil
-		4.times do
-			if my_ticket[i] == winning_tickets[0][i]			
-				c += 1
-			end
-			i += 1
+	4.times do
+		if my_ticket[i] == winning_tickets[0][i]			
+			c += 1
 		end
+		i += 1
 	end
 	if c == 3
 		true
@@ -16,3 +13,25 @@ def off(my_ticket, winning_tickets)
 		false
 	end
 end
+
+
+
+###### just trying something ######
+# def off(my_ticket, winning_tickets)
+# 	i = 0
+# 	c = 0
+# 	winning_tickets.each do |element|
+# 		next if element = nil
+# 		4.times do
+# 			if my_ticket[i] == winning_tickets[0][i]			
+# 				c += 1
+# 			end
+# 			i += 1
+# 		end
+# 	end
+# 	if c == 3
+# 		true
+# 	else
+# 		false
+# 	end
+# end
